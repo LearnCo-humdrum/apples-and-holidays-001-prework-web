388f557a05ba0b3ede435f534767aa743b85052f
jjrajani <jjrajani@gmail.com>
2016-04-20 14:09:13 -0400

    Done.

---
+++ lib/untitled.rb
@@ -0,0 +1,37 @@
+holiday_hash = {
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     }
+   }
+
+def all_supplies_in_holidays(holiday_hash)
+  # iterate through holiday_hash and print items such that your readout resembles:
+  # Winter:
+  #   Christmas: Lights, Wreath
+  #   New Years: Party Hats
+  # Summer:
+  #   Fourth Of July: Fireworks, BBQ
+  # etc.
+  
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    array = holiday.collect do |holiday, supply|
+               holiday.to_s.capitalize
+            end
+    supplies = holiday.collect do |holiday, supply|
+      supply.to_s.capitalize
+    end
+  end
+
+end
+all_supplies_in_holidays(holiday_hash)
